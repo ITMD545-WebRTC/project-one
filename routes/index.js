ebb5ff7a744e9e333b3ed46944e6ad03ee2134c5
Mohmedamir <marther@hawk.iit.edu>
2020-09-24 10:03:05 -0500

    add file system (fs) to write scrapped data into new file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,20 +23,26 @@ async function run() {
       let updates = document.querySelector("span.news_category_title").innerText;
       let firstUpdate = document.querySelector("div#newsdate2020-09-23").innerText;
       let secondUpdate = document.querySelector("div#newsdate2020-09-22").innerText;
-      
+
       return {
         news, newsDate, updates, firstUpdate, secondUpdate
       }
-      
+
     });
 
     console.log(result);
+    var fs =require('fs');
+
+    fs.writeFile('../views/worldometers_data.txt', JSON.stringify(result),['utf8'],function(err){
+      if (err) throw (err);
+      console.log('Data Saved in File');
+    });
   } catch (error) {
     console.error("Error: " +error.message);
   } finally {
     await browser.close();
   }
-  
+
 }
 
 run();
