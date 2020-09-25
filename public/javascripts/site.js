5c3b5f5a5cf829be1c505153109569f7e50ff3af
Cristian Pintor <cpintor@hawk.iit.edu>
2020-09-25 02:52:36 -0500

    Copying code into app.js
    - copying read file and dif code from /routes/index.js into app.js
    - adding depedencies to app.js
    - there is an issue with fileEvent bc the changes are not showing up on the browser's console

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,4 +2,5 @@ var socket = io.connect('/');
 
 socket.on('message', function(data) {
   console.log('Message received: ' + data);
+  socket.emit('message received', 'I got your message');
 });
--- routes/index.js
+++ routes/index.js
@@ -18,15 +18,15 @@ async function run() {
 
   try {
     // readFile worldometers_data.txt and find diffs and output to console
-        var old_file = fs.readFileSync('../views/worldometers_data.txt', {encoding:"utf8"});
+        var old_file = fs.readFileSync('./views/worldometers_data.txt', {encoding:"utf8"});
         var fileEvent = new EventEmitter();
 
         fileEvent.on('changed file', function(data){
           console.log('The file was changed and fired an event. This data was received:\n' + data);
         });
 
-        fs.watch('../views/worldometers_data.txt', function(eventType, filename) {
-          fs.promises.readFile(`../views/${filename}`, {encoding:"utf8"})
+        fs.watch('./views/worldometers_data.txt', function(eventType, filename) {
+          fs.promises.readFile(`./views/${filename}`, {encoding:"utf8"})
             .then(function(data) {
             // only flash this message if the file's content has changed
             var new_file = data;
@@ -75,7 +75,7 @@ async function run() {
     //console.log(parsedResult);
     var formatResult = strResult.replace(/\\n/g, '\n');
     console.log(formatResult);
-    fs.writeFile('../views/worldometers_data.txt', formatResult,{encoding:"utf-8"},function(err){
+    fs.writeFile('./views/worldometers_data.txt', formatResult,{encoding:"utf-8"},function(err){
     if (err) throw (err);
     console.log('Newly fetched data Saved in File');
 
