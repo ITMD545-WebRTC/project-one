6c7f4b018b15d30e797512c91cf2012916c27bfe
Cristian Pintor <cpintor@hawk.iit.edu>
2020-09-26 23:35:48 -0500

    removing lines 72 to 101

--- app.js
+++ app.js
@@ -69,36 +69,6 @@ async function run() {
           });
           }
         );
-    // fetch website using puppeteer
-    const page = await browser.newPage();
-    await page.goto("https://www.worldometers.info/coronavirus/#news");
-    // grab and return new data from website
-    const result = await page.evaluate(() => {
-      let header = document.querySelector("div.content-inner");
-      let glbData = document.querySelector("#maincounter-wrap").innerText;
-      let news = document.querySelector("h2").innerText;
-      let newsDate = document.querySelector("h4").innerText;
-      let updates = document.querySelector("span.news_category_title").innerText;
-      let firstUpdate = document.querySelector("div#newsdate2020-09-23").innerText;
-      let secondUpdate = document.querySelector("div#newsdate2020-09-22").innerText;
-
-      return {
-        glbData, news, newsDate, updates, firstUpdate, secondUpdate
-      }
-
-    });
-    // take scrapped data and write it to a file
-    var strResult = JSON.stringify(result);
-    //console.log(strResult);
-    var parsedResult = JSON.parse(strResult);
-    //console.log(parsedResult);
-    var formatResult = strResult.replace(/\\n/g, '\n');
-    console.log(formatResult);
-    fs.writeFile('views/worldometers_data.txt', formatResult,{encoding:"utf-8"},function(err){
-    if (err) throw (err);
-    console.log('Newly fetched data Saved in File');
-
-    });
 
   } catch (error) {
     console.error("Error: " +error.message);
